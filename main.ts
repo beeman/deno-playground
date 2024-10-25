e75ab2a980021db97a3794bf8544da22dd716778
Bram Borggreve <beeman@beeman.nl>
2024-10-25 01:19:38 -0500

    chore: initial commit

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 export function add(a: number, b: number): number {
-  return a + b;
+  return a + b
 }
+
+import { Hono } from '@hono/hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Deno!')
+})
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  const pairs: [number, number][] = [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+    [7, 8],
+    [9, 10],
+  ]
+  for (const pair of pairs) {
+    console.log('Add', pair[0], '+', pair[1], '=', add(pair[0], pair[1]))
+  }
+  // console.log('Add 2 + 3 =', add(2, 3))
+  // console.log('Add 2 + 3 =', add(2, 3))
+  // console.log('Add 2 + 3 =', add(2, 3))
+  // console.log('Add 2 + 3 =', add(2, 3))
+  // console.log('Add 2 + 3 =', add(2, 3))
+  Deno.serve(app.fetch)
 }
